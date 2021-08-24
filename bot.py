cead41c8e7fe7ea1e9d1657f451484ac73cfed85
fennr <fenrir1121@gmail.com>
2021-08-24 08:49:04 +0500

    test postgre

--- bot.py
+++ bot.py
@@ -14,6 +14,8 @@ import sys
 import logging
 import traceback
 
+import psycopg2
+
 import discord
 import yaml
 from discord.ext import commands, tasks
@@ -27,6 +29,35 @@ if not os.path.isfile("config.yaml"):
 else:
     with open("config.yaml") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
+
+# read database connection url from the enivron variable we just set.
+DATABASE_URL = os.environ.get('DATABASE_URL')
+con = None
+try:
+    # create a new database connection by calling the connect() function
+    con = psycopg2.connect(DATABASE_URL)
+
+    #  create a new cursor
+    cur = con.cursor()
+
+    # execute an SQL statement to get the HerokuPostgres database version
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+
+    # close the communication with the HerokuPostgres
+    cur.close()
+except Exception as error:
+    print('Cause: {}'.format(error))
+
+finally:
+    # close the communication with the database server by calling the close()
+    if con is not None:
+        con.close()
+        print('Database connection closed.')
 
 """	
 Setup bot intents (events restrictions)
