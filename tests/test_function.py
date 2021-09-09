6d45ff212e34876bd063c759f97ccde0092e9062
fennr <fenrir1121@gmail.com>
2021-09-10 00:03:57 +0500

    add tests
    Добавил тесты для фукнций и нексускомпендиума

---
+++ tests/test_function.py
@@ -0,0 +1,23 @@
+import pytest
+from hots import function
+from hots.Hero import Hero
+
+
+class Test:
+    good_name = 'Самуро'
+    bad_name = 'Cомуро'
+
+    def test_read_hero_from_message(self):
+        try:
+            hero, embed = function.read_hero_from_message(self.bad_name, author='fenrir#5455', command='hero')
+        except:
+            pytest.fail('Считывание героя ушло в дамп')
+        assert type(hero) is Hero
+
+    def test_find_heroes_list(self):
+        hero_list = function.find_heroes(hero_name=self.bad_name)
+        assert hero_list is not []
+
+    def test_find_heroes_types(self):
+        hero_list = function.find_heroes(hero_name=self.bad_name)
+        assert type(hero_list[0]) is Hero
