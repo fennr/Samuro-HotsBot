de0b53b2195650aa7d7f64562d1d99c66bf19518
Fennr <fenrir1121@gmail.com>
2022-03-06 13:21:38 +0500

    test flask

---
+++ flask_site/__init__.py
@@ -0,0 +1,8 @@
+from flask_site import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_view():
+    return "<h1>Test web app</h1>"
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from flask_site import app
+
+if __name__ == "__main__":
+    app.run()
