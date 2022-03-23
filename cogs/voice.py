066a424372d9bec4a62baeb23515ba51c83695ce
Fennr <fenrir1121@gmail.com>
2022-03-23 09:49:49 +0500

    add help

--- cogs/voice.py
+++ cogs/voice.py
@@ -78,6 +78,23 @@ class voice(commands.Cog):
         pass
 
     @voice.command()
+    async def help(self, ctx):
+        embed = discord.Embed(title="Help", description="", color=0x7289da)
+        embed.set_author(name=f"{ctx.guild.me.display_name}", url="https://discordbots.org/bot/472911936951156740",
+                         icon_url=f"{ctx.guild.me.avatar_url}")
+        embed.add_field(name=f'**Commands**',
+                        value=f'**Lock your channel by using the following command:**\n\n`.voice lock`\n\n------------\n\n'
+                              f'**Unlock your channel by using the following command:**\n\n`.voice unlock`\n\n------------\n\n'
+                              f'**Change your channel name by using the following command:**\n\n`.voice name <name>`\n\n**Example:** `.voice name EU 5kd+`\n\n------------\n\n'
+                              f'**Change your channel limit by using the following command:**\n\n`.voice limit number`\n\n**Example:** `.voice limit 2`\n\n------------\n\n'
+                              f'**Give users permission to join by using the following command:**\n\n`.voice permit @person`\n\n**Example:** `.voice permit @Sam#9452`\n\n------------\n\n'
+                              f'**Claim ownership of channel once the owner has left:**\n\n`.voice claim`\n\n**Example:** `.voice claim`\n\n------------\n\n'
+                              f'**Remove permission and the user from your channel using the following command:**\n\n`.voice reject @person`\n\n**Example:** `.voice reject @Sam#9452`\n\n',
+                        inline='false')
+        embed.set_footer(text='Bot developed by Sam#9452')
+        await ctx.channel.send(embed=embed)
+
+    @voice.command()
     async def setup(self, ctx):
         conn = sqlite3.connect('voice.db')
         c = conn.cursor()
