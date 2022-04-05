66eff9aa645fe37bda8b69d8cc217e95f25f09e5
Fennr <fenrir1121@gmail.com>
2022-04-05 13:16:32 +0500

    test print

--- utils/library/profile.py
+++ utils/library/profile.py
@@ -26,7 +26,9 @@ async def add_role(ctx, player: Player, role_name='5x5'):
         print(type(player))
         print(player.id)
         member = ctx.guild.get_member(player.id)
+        print(member)
         role = utils.get(member.guild.roles, name=role_name)
+        print(role)
         await member.add_roles(role)
         await ctx.send(f"Присвоена роль *{player.league}*. Добро пожаловать на сервер.")
     except Exception as e:
