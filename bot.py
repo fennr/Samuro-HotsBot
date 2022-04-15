bcbeff32991a6db334829df4a08126380092a277
Fennr <fenrir1121@gmail.com>
2022-04-15 22:59:48 +0500

    add date update

--- bot.py
+++ bot.py
@@ -46,6 +46,8 @@ async def on_ready():
     print(f"Discord.py API version: {discord.__version__}")
     print(f"Python version: {platform.python_version()}")
     print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print(f"Current version: {config.version}")
+    print(f"Last update: {config.update}")
     print("-------------------")
     status_task.start()
 
